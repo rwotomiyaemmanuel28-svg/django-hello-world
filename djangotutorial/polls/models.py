14dd3e824973a252f868068a819c04aabc094ca2
rwotomiyaemmanuel28-svg <rwotomiyaemmanuel28@gmail.com>
2026-07-28 17:37:07 -0500

    Committing again

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,5 +1,5 @@
 import datetime
-
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -15,7 +15,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
